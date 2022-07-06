29419dc628e67e48724e39771530af38e6c3f971
satinder <satindersingh472@gmail.com>
2022-07-05 21:01:12 -0600

    added else if

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let userSpouse = "simran";
 let userAge = 27;
 let userSmoke = false;
 let userIncome = 5000;
-let userPoints=7;
+let userPoints= 60;
 
 console.log(userName);
 console.log(userSpouse);
@@ -12,6 +12,9 @@ console.log(userSmoke);
 console.log(userIncome);
 if (userPoints >= 75){
     console.log("congratulations to the user on getting passing points");
-} else {
+} else if (userPoints >= 50) {
+    console.log("the user atleast still qualified because score is more than 50");
+} 
+else {
     console.log("the user will make it next time for sure!!!")
 }
