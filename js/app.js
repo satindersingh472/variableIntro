177ba8848e95c6e5bbdfaf77a3bfa3f47843f472
satinder <satindersingh472@gmail.com>
2022-07-05 20:39:54 -0600

    added

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let userName ="satinder";
+let userSpouse = "simran";
+let userAge = 27;
+let userSmoke = false;
+let userIncome = 5000;
+let userPoints= 99;
+if (userPoints >= 75){
+    console.log("congratulations to the user on getting passing points");
+};
+console.log(userName);
+console.log(userSpouse);
+console.log(userAge);
+console.log(userSmoke);
+console.log(userIncome);
