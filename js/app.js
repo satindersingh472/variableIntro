1d7460a9820107df910b8bbdc08ee57e8f9c1a8d
satinder <satindersingh472@gmail.com>
2022-07-05 20:49:33 -0600

    added else statement

--- js/app.js
+++ js/app.js
@@ -3,12 +3,15 @@ let userSpouse = "simran";
 let userAge = 27;
 let userSmoke = false;
 let userIncome = 5000;
-let userPoints= 99;
-if (userPoints >= 75){
-    console.log("congratulations to the user on getting passing points");
-};
+let userPoints=7;
+
 console.log(userName);
 console.log(userSpouse);
 console.log(userAge);
 console.log(userSmoke);
 console.log(userIncome);
+if (userPoints >= 75){
+    console.log("congratulations to the user on getting passing points");
+} else {
+    console.log("the user will make it next time for sure!!!")
+}
