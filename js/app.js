d056bfbbd37053f7219927f08a3c5b43d3a448fb
satinder <satindersingh472@gmail.com>
2022-07-07 16:58:09 -0600

    finished knowledge check

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-let userName ="satinder";
+let userName ="alex";
 let userSpouse = "simran";
 let userAge = 27;
 let userSmoke = false;
 let userIncome = 5000;
-let userPoints= 60;
+let userPoints= 11;
 
 console.log(userName);
 console.log(userSpouse);
@@ -17,4 +17,18 @@ if (userPoints >= 75){
 } 
 else {
     console.log("the user will make it next time for sure!!!")
+}
+
+let isMember = true;
+if (isMember === true && userPoints >= 90){
+    console.log("special message , you are a member and you scored above 90");
+} else if (isMember === false || userPoints <=10){
+    console.log("Hey user,  you must leave you are looser");
+} else{
+    console.log("the user is a member but he needs to work hard")
+}
+if (userName === "alex"){
+    console.log("user you are great person");
+} else {
+    console.log("this is just a regular user");
 }
